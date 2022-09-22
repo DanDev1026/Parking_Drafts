b567c24b9498fa7f66b1bd9dc42945627ffc4315
Beker Martinez <esteman500@gmail.com>
2022-09-22 15:58:10 -0500

    prueba

--- Drafts.App.Dominio/Entidades/Vigilante.cs
+++ Drafts.App.Dominio/Entidades/Vigilante.cs
@@ -6,6 +6,7 @@ namespace Drafts.App.Dominio
     {
         public int Id { get; set; }
         public string Pisos { get; set; }
+        public string HoraIngreso {get; set;}
         public string Zona { get; set; }
         public string Distribucion_AlfaNum { get; set; }
         public string Espacio_Libre { get; set; }
