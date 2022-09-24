dc65e883566b3c264da82ee2c70bd72c7bd8b514
Carlos Bohorquez <bohorquezguevaracarlosalberto@gmail.com>
2022-09-24 16:26:00 -0500

    actualizacion

---
+++ Drafts.App.Persistencia/Migrations/20220924212525_frontenddos.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Drafts.App.Persistencia.Migrations
+{
+    public partial class frontenddos : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
